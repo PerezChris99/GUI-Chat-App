10e4e09f799da8f6b9f28384974b7dbad9875f89
perezchris3599 <kweeziperez712@gmail.com>
2023-02-06 16:50:14 +0300

    server

--- server.py
+++ server.py
@@ -12,3 +12,48 @@ server.listen()
 clients = []
 nicknames = []
 
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle_client(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f"{nicknames[clients.index(client)]} says: {message}")
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+""""
+def receive(client):
+    while True:
+        data = client.recv(1024)
+        if not data:
+            break
+        broadcast(data)
+"""
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}!")
+
+        client.send("PERRY".encode('utf-8'))
+        nickname = client.recv(1024)
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f"{nickname} connected to the server!\n".encode('utf-8'))
+        client.send("Connected to the server".encode('utf-8'))
+
+        thread = threading.Thread(target=receive, args=(client,))
+        thread.start()
+
+print("Server running.......")
+receive()
