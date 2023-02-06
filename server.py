b7968dbc4bab378cca517623dbc1cbd878d1db60
perezchris3599 <kweeziperez712@gmail.com>
2023-02-06 16:36:12 +0300

    initial commit

---
+++ server.py
@@ -0,0 +1,14 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 9090
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+clients = []
+nicknames = []
+
